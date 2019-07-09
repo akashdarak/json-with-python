c9eba8231882f16cbd110da1db5a5be220ea5f72
Akash Darak <akashdarak@gmail.com>
2019-07-09 15:07:15 -0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,6 +6,7 @@ import requests
 def rate(succ, req):
   succ = float(succ)
   req = float(req)
+  
   #Calculate the percentage upto 2 decimals
   succ_rate = '{0:.2f}'.format((succ / req * 100))
   return succ_rate
@@ -26,6 +27,7 @@ if __name__=='__main__':
   with open("servers.txt") as f:
     for line in f:
       resp = requests.get(line)
+      
       #The requests library should return a dictionary similar to the below:
       #resp = {"Application":"Cache2","Version":"1.0.1","Uptime":4637719417,
       #        "Request_Count":5194800029,"Error_Count":1042813251,"Success_Count":4151986778}
@@ -34,11 +36,13 @@ if __name__=='__main__':
 
       succ = resp["Success_Count"]
       req = resp["Request_Count"]
+      
       #Function to calculate success rate
       succ_rate = rate(succ, req)
 
       app = resp["Application"]
       ver = resp["Version"]
+      
       #Function to print to standard output and file
       out_file(app, ver, succ_rate)
 
